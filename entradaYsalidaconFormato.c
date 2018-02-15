9988dea1d5fdcfb84aeca3effca246c5f8bff4d7
verona <lau.marzet@gmail.com>
2018-02-15 14:12:18 -0600

    relleno de campos

--- entradaYsalidaconFormato.c
+++ entradaYsalidaconFormato.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*****************
+
+% banderas  ancho   precision   tamaño   caracter
+
+# formato hexadecimal
+printf  
+******************/
 int main()
 {
     //variables enteras
@@ -12,16 +19,45 @@ int main()
     char cadena[]="hola mundo";
     long long int entero_muy_muy_largo=1234567891011LL;
 
-    printf("%d\n", entero_muy_muy_largo);
-    printf("%lld\n", entero_muy_muy_largo);
-    printf("%o\n", octal);
-    printf("%x\n", hexadecimal);
-    printf("%X\n", hexadecimal);
-    printf("%d\n", hexadecimal);
+    // printf("%d\n", entero_muy_muy_largo);
+    // printf("%lld\n", entero_muy_muy_largo);
+    // printf("%o\n", octal);
+    // printf("%#x\n", hexadecimal);
+    // printf("%#X\n", hexadecimal);
+    // printf("%d\n", hexadecimal);
 
+
+    int resultado;
+    
+    // resultado=printf("%c\n",caracter);
+    // printf("%d\n",resultado);
+
+    // resultado=   printf("%s\n",cadena);
+    // printf("%d\n", resultado);
+
+    // puts(cadena);
+    // putchar(caracter);
+
+
+    //variables de punto flotante
+    // printf("%e\n",1234567.89);
+    // printf("%e\n", +1234567.89);
+    // printf("%e\n", -1234567.89);
+    // printf("%E\n",1234567.89);
+    // printf("%.2f\n",1234567.89);
+    // printf("%g\n",1234567.89);
+    // printf("%G\n",1234567.89);
+    
+
+
+
+    printf("%04d\n",1); //para rellenar el ancho del campo cuando no hay espacio 
+    printf("%+4d\n",12);   //+ hace que agrega el signo al numero (+positivo, - negativo) 
+    printf("%4d\n",123);    
+    printf("%4d\n",1234);
+    printf("%4d\n",12345);
     
     
-
 
 
 
