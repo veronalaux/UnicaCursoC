7cb5fc6e40d08f8a8cf257e6be844eb3f8a1d11c
verona <lau.marzet@gmail.com>
2018-02-16 12:37:18 -0600

    si lee una letra, ahi termina si lee un espacio, lo corta y loo demas lo asigna a la otra variable

---
+++ scanfSintaxis.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n1,n2;
+
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    
+    printf("%d\n", n1);
+    printf("%d\n", n2);
+
+
+    return EXIT_SUCCESS;
+}
